1b1cd25b2891402f65221c076915b4042227304e
Ashanti Hatchett <ahatchett02@gmail.com>
2025-01-29 17:41:19 +0000

    added form for user to input dice information and be taken to a page of the number of rolls of their selected dice

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/process_roll") do
+  @number_of_dice = params.fetch("dice").to_i
+  @number_of_sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @number_of_dice.times do
+    roll = rand(1..@number_of_sides)
+    @rolls.push(roll)
+  end
+
+  erb(:process_roll)
 end
